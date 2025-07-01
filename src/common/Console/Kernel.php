805645c2d3d60415642a58e117dd299ab8b88c62
kunyu <1960049949@qq.com>
2025-07-01 13:47:23 +0800

    增加Clockwork日志清理

--- src/common/Console/Kernel.php
+++ src/common/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace hhttp\io\common\Console;
 
 use hhttp\io\common\Console\Command\LogClean;
 use Illuminate\Console\Scheduling\Schedule;
+use Clockwork\Support\Laravel\ClockworkCleanCommand;
 
 class Kernel
 {
@@ -16,8 +17,13 @@ class Kernel
     public function schedule(Schedule $schedule)
     {
         try {
+
            // 本定时任务的作用是清理过期日志文件 [api;hhttp;sql] 每天执行一次
             $schedule->command(LogClean::class)->daily();
+
+            // 本定时任务的作用是清理过期日志文件
+            // 具体时间配置见env配置中的CLOCKWORK_STORAGE_EXPIRATION项 默认7天
+            $schedule->command(ClockworkCleanCommand::class)->everyMinute();
         }catch (\Throwable $e){}
     }
 }
